d8bba3c185ed23c6d63f27d69a7493e0796947eb
mahtamzz <mahtaieam@gmail.com>
2025-12-11 14:54:00 +0330

    JWT service fixed

--- Backend/IAM/src/infrastructure/auth/JwtService.js
+++ Backend/IAM/src/infrastructure/auth/JwtService.js
@@ -3,14 +3,21 @@ const env = require("../../config/env");
 
 class JwtService {
     generate(payload) {
-        return jwt.sign(payload, env.JWT_KEY, {
-            expiresIn: "1h",
-        });
+        return jwt.sign(payload, env.JWT_KEY, { expiresIn: "1h" });
     }
 
     verify(token) {
         return jwt.verify(token, env.JWT_KEY);
     }
+
+    generateRefreshToken(payload) {
+        return jwt.sign(payload, env.JWT_KEY, { expiresIn: "7d" });
+    }
+
+    verifyRefresh(token) {
+        return jwt.verify(token, env.JWT_KEY);
+    }
 }
+
 
 module.exports = new JwtService();
